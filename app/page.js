f44590fbc4402ac356ac99fb5f37a7eff38ea5fa
Aniket <vishnoianiket4@gmail.com>
2024-03-10 19:57:10 +0530

    hero page done(framer motion left)

--- app/page.js
+++ app/page.js
@@ -2,7 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <>
-   </>
+    <div className="h-full flex justify-center px-4 lg:flex-row  sm:px-8 md:px-12 lg:px-20 xl:px-48 flex-col">
+      {/* image container */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <Image src='/me.png' fill className="object-cover"/>
+      </div>
+      {/* text container */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-5 items-center justify-center">
+        <h1 className="text-3xl md:text-5xl font-bold ">FrontEnd Developer</h1>
+        <p className="md:text-base ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe dolor, sequi reprehenderit, maiores atque 
+        qui nemo nostrum quia, dolorum facilis eos voluptatibus cumque vel accusantium omnis rerum doloribus quasi laborum!</p>
+      
+        <div className="flex gap-3 w-full "> 
+          <button className="p-2 rounded ring-1 bg-black text-white ring-black">View My Work</button>
+          <button className="p-2 rounded ring-1 ring-black text-sm">Contact Me</button>
+        </div>
+      </div>
+      
+    </div>
   );
 }
