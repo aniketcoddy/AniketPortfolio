7f2c70eaed0a0f42a49d9fb16cca1c68ae4cf4c5
Aniket <vishnoianiket4@gmail.com>
2024-03-10 20:04:33 +0530

    responsive hero section

--- app/page.js
+++ app/page.js
@@ -8,14 +8,14 @@ export default function Home() {
         <Image src='/me.png' fill className="object-cover"/>
       </div>
       {/* text container */}
-      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-5 items-center justify-center">
-        <h1 className="text-3xl md:text-5xl font-bold ">FrontEnd Developer</h1>
-        <p className="md:text-base ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe dolor, sequi reprehenderit, maiores atque 
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-3 md:gap-5 items-center justify-center">
+        <h1 className="text-2xl md:text-5xl font-bold ">FrontEnd Developer</h1>
+        <p className="md:text-base text-xs">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe dolor, sequi reprehenderit, maiores atque 
         qui nemo nostrum quia, dolorum facilis eos voluptatibus cumque vel accusantium omnis rerum doloribus quasi laborum!</p>
       
         <div className="flex gap-3 w-full "> 
-          <button className="p-2 rounded ring-1 bg-black text-white ring-black">View My Work</button>
-          <button className="p-2 rounded ring-1 ring-black text-sm">Contact Me</button>
+          <button className="p-2  rounded ring-1 bg-black text-white ring-black text-sm">View My Work</button>
+          <button className="p-2  rounded ring-1 ring-black text-sm">Contact Me</button>
         </div>
       </div>
       
