f60d72b511d0c5dce1b4858650c8e33887cee70b
Aniket <vishnoianiket4@gmail.com>
2024-03-11 16:50:40 +0530

    home page and navbar done with animation

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,15 +3,72 @@ import Image from "next/image";
 import React, { useState } from "react";
 import Link from "next/link";
 import NavLink from "./NavLink";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+
+  const topVariants = {
+    closed: {
+      rotate: 0,
+    },
+
+    opened: {
+      rotate: 45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+
+  const centerVariants = {
+    closed: {
+      opacity: 1,
+    },
+
+    opened: {
+      opacity: 0,
+    },
+  };
+
+  const lastVariants = {
+    closed: {
+      rotate: 0,
+    },
+
+    opened: {
+      rotate: -45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+
+  const listVariants = {
+    closed: {
+      x: "100vw",
+    },
+    opened: {
+      x: 0,
+      transition:{
+        when:"beforeChildren",
+        staggerChildren:0.2,
+      }
+    },
+  };
+
+  const listItemVariants = {
+    closed: {
+      x: "-10",
+      opacity:0,
+    },
+    opened: {
+      x: 0,
+      opacity:1,
+    },
+  };
 
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
       <div className="hidden md:flex gap-4 w-1/3 md:justify-center text-lg ">
         {NavLinks.map((link) => (
-          <NavLink link={link} key={link.title}/>
+          <NavLink link={link} key={link.title} />
         ))}
       </div>
 
@@ -46,31 +103,58 @@ const Navbar = () => {
           className="w-10 h-8 flex flex-col justify-between z-50 relative"
           onClick={() => setOpen(!open)}
         >
-          <div className="w-10 h-1 bg-white rounded"></div>
-          <div className="w-10 h-1 bg-white rounded"></div>
-          <div className="w-10 h-1 bg-white rounded"></div>
+          <motion.div
+            variants={topVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black  rounded origin-left"
+          ></motion.div>
+          <motion.div
+            variants={centerVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded"
+          ></motion.div>
+          <motion.div
+            variants={lastVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded origin-left"
+          ></motion.div>
         </button>
 
         {/* Menu Items */}
         {open && (
-          <div className="absolute bg-black w-screen text-white top-0 left-0 h-screen flex flex-col items-center justify-center gap-8 text-2xl">
+          <motion.div
+            variants={listVariants}
+            initial="closed"
+            animate="opened"
+            className="absolute bg-black w-screen text-white top-0 left-0 h-screen flex flex-col items-center justify-center gap-8 text-2xl z-40"
+          >
             {NavLinks.map((links) => (
-              <Link href={links.url} key={links.title}>
-                {links.title}
-              </Link>
+              <motion.div variants={listItemVariants} key={links.title}>
+                <Link href={links.url}>{links.title}</Link>
+              </motion.div>
             ))}
             <div className="flex gap-5">
               <Link href="/">
                 <Image src="/github2.png" alt="github" width={30} height={30} />
               </Link>
               <Link href="/">
-                <Image src="/linkedin.png" alt="linkidin" width={30} height={30} />
+                <Image
+                  src="/linkedin.png"
+                  alt="linkidin"
+                  width={30}
+                  height={30}
+                />
               </Link>
               <Link href="/">
-                <Image src="/whatsapp.png" alt="whatsapp" width={30} height={30} />
+                <Image
+                  src="/whatsapp.png"
+                  alt="whatsapp"
+                  width={30}
+                  height={30}
+                />
               </Link>
             </div>
-          </div>
+          </motion.div>
         )}
       </div>
     </div>
